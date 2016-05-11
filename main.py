ef8d4bb91f0bae16941f1fffcfce247e4ac70179
Jeremiah Lantzer <jeremiahlantzer25@gmail.com>
2016-05-11 13:35:10 -0400

    Added in more basic functionality including basic validation function.

--- main.py
+++ main.py
@@ -9,15 +9,36 @@ individual GPAs and cumulative GPAs.
 
 Purpose: Summer Project week #1
 
-Variables: gpas[], count, response
+Variables: gpa[], count, response, continueVar
 
-Functions: checkValid(to be added later, will check the validity of response)
+Functions: checkvalid(to be added later, will check the validity of response)
 """
 
-gpas = list()  # currently empty list will hold all inputted GPAs
+gpa = list()  # currently empty list will hold all inputted GPAs
 count = 0  # count will track how many GPAs have been entered
 response = 'y'  # will be used to check whether the user wants to add another GPA to the list
+continueVar = True
 
-while response == 'y':
 
-    count += 1
+# function will check to make sure the user continue input is valid
+def checkvalid(resp):
+    if resp == 'y' or resp == 'Y':  # if y or Y is entered the program continues
+        return True
+    elif resp == 'n' or resp == 'N':  # if n or N is entered the program stops
+        return False
+    else:  # anything else must be reentered
+        return 0
+
+
+while continueVar:  # loop will run as long as continueVar is true
+
+    gpa[count] = float(
+        input('Please enter a GPA: '))  # asks the user for a GPA. stores it as a float types in the list
+    print
+
+    response = input('Would you like to continue? y/n: ')
+    print
+    continueVar = checkvalid(response)
+    # TODO: add in loop to check result again
+
+    count += 1  # count increases after each iteration to cycle through the GPA list
